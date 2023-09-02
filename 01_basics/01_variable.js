931b2b2b1b15361c83dc900692bb73a492f417f4
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-02 14:27:49 +0000

    Investigate Javascript Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 14456;
+let accountEmail = "imran112@gmail.com";
+var accountPassword = "45678";
+
+accountCity = "Lahore";
+let accountState;
+
+//accountId = 12345 / not allowed
+
+accountEmail = "khan123@gmail.com"
+accountPassword= "786786"
+accountCity ="Islamabad"
+
+console.log(accountId)
+
+/*
+preder not to use var
+because of issue in block scope and function scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState] )
