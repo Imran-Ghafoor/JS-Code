c24aaa7b458ef1cb6cafdf780cf79c4c76e3e4e3
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-04 19:27:33 +0500

    Conversion Datatypes

---
+++ 01_basics/03_ConversionOperayion.js
@@ -0,0 +1,24 @@
+let score = "imran"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0
+
+let isLogedIn = "khan"
+let booleanIsLogedIn = Boolean(isLogedIn)
+//console.log(booleanIsLogedIn)
+
+// 1 => true ; 0 => false
+// "" empty-string => false
+// "khan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
