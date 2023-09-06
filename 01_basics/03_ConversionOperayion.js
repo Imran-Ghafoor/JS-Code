8ea71f433810ed58a6288d77c707678d7b801c8a
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-06 18:49:57 +0500

    Investigate Conversion

--- 01_basics/03_ConversionOperayion.js
+++ 01_basics/03_ConversionOperayion.js
@@ -22,3 +22,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *****************************Opertaions******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " imran"
+// console.log(str1+str2);
+
+//Problem is here>>
+
+// console.log("1" + 2);  //>>12
+// console.log(1 + "2");  //>>12
+// console.log("1" + 2+3); //>>123
+// console.log(1 + 2+ "3"); // >>33
+
+// console.log(+ true); // usable
+// console.log(true +); //not use
+
+let gameCounter = 100
+gameCounter++ ;
+++gameCounter;   // prefix postfix  prefix>> increment value first >> postfix>> value increment after search
+console.log(gameCounter);
