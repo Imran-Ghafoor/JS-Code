360eed181f30850a68eae7565ea87995fe4fdf88
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-07 03:33:17 +0500

    Update datatype_summary.js

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -2,6 +2,16 @@
 
 // 7 types : string, Number, Boolean, null, undefined, symbal, BigInt
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123)    // symbol use in js liberary or framework for uniqueness
+const anotherId = Symbol('123')
+
 
 //Renfrence (Non Primitive)
 
