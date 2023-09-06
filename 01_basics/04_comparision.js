86d79f5cfdd1cd880566f742647a798f38cac206
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-09-06 20:19:13 +0500

    Investigate comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// The Problem is here //
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// Avoid this type of comparisions //
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+//   ------------------------- //
+
+// === strict check check your data-type also
+console.log("2" === 2);
+
